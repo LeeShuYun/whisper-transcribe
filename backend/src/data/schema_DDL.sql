17226037c24072d863e23c65de83593d3d8baa61
LeeShuYun <107480969+LeeShuYun@users.noreply.github.com>
2024-11-08 11:42:45 +0800

    fixed sqlite triggers in DDL

--- backend/src/data/schema_DDL.sql
+++ backend/src/data/schema_DDL.sql
@@ -1,6 +1,10 @@
 -- SQLite
-DROP TABLE IF EXISTS transcriptions;
+-- DROP TABLE IF EXISTS transcriptions;
 DROP TABLE IF EXISTS transcriptions_idx;
+DROP TRIGGER IF EXISTS transcribe_insert;
+DROP TRIGGER IF EXISTS transcribe_delete;
+DROP TRIGGER IF EXISTS transcribe_update;
+
 CREATE TABLE IF NOT EXISTS transcriptions
 (
 	id INTEGER NOT NULL PRIMARY KEY,
@@ -24,26 +28,27 @@ CREATE TRIGGER transcribe_insert AFTER INSERT ON transcriptions BEGIN
  VALUES (new.id, new.created_on, new.audio_file_name, new.transcription);
 END;
 
-CREATE TRIGGER transcribe_delete AFTER DELETE ON transcriptions BEGIN
+CREATE TRIGGER transcribe_delete AFTER DELETE ON transcriptions FOR EACH ROW
+BEGIN
+    DELETE FROM transcriptions_idx WHERE created_on = OLD.created_on;
+END
+
+CREATE TRIGGER transcribe_update AFTER UPDATE ON transcriptions FOR EACH ROW
+BEGIN
+    DELETE FROM transcriptions_idx WHERE created_on = OLD.created_on;
   INSERT INTO transcriptions_idx(rowid, created_on, audio_file_name, transcription)
- VALUES('delete', OLD.created_on, old.audio_file_name, old.transcription);
-END;
-CREATE TRIGGER transcribe_update AFTER UPDATE ON transcriptions BEGIN
-  INSERT INTO transcriptions_idx(rowid, created_on, audio_file_name, transcription)
- VALUES('delete', old.created_on, old.audio_file_name, old.transcription);
-  INSERT INTO transcriptions_idx(rowid, created_on, audio_file_name, transcription)
- VALUES (new.created_on, new.created_on, new.audio_file_name, new.transcription);
+ VALUES (new.id, new.created_on, new.audio_file_name, new.transcription);
 END;
 
 --insert test data
-INSERT INTO transcriptions (created_on, audio_file_name, transcription)
-VALUES ((strftime('%Y-%m-%d %H:%M:%S:%s')), "Sample 0", "This is a placeholder");
+-- INSERT INTO transcriptions (created_on, audio_file_name, transcription)
+-- VALUES ((strftime('%Y-%m-%d %H:%M:%S:%s')), "Sample 0", "This is a placeholder");
 
-INSERT INTO transcriptions (created_on, audio_file_name, transcription)
-VALUES (strftime('%Y-%m-%d %H:%M:%S:%s'), "Sample 000", "This is a placeholder");
+-- INSERT INTO transcriptions (created_on, audio_file_name, transcription)
+-- VALUES (strftime('%Y-%m-%d %H:%M:%S:%s'), "Sample 000", "This is a placeholder");
 
-INSERT INTO transcriptions (created_on, audio_file_name, transcription)
-VALUES (strftime('%Y-%m-%d %H:%M:%S:%s'), "SampleTEST", "This is a placeholder");
+-- INSERT INTO transcriptions (created_on, audio_file_name, transcription)
+-- VALUES (strftime('%Y-%m-%d %H:%M:%S:%s'), "SampleTEST", "This is a placeholder");
 
 -- select * from transcriptions
 
@@ -52,6 +57,6 @@ VALUES (strftime('%Y-%m-%d %H:%M:%S:%s'), "SampleTEST", "This is a placeholder")
 -- SELECT * FROM transcriptions_idx
 -- WHERE transcriptions_idx MATCH 'sample NOT 000' ORDER BY rank;
 
-SELECT rank, t.created_on, t.audio_file_name, t.transcription FROM transcriptions t
-JOIN transcriptions_idx i ON t.ROWID = i.ROWID
-WHERE transcriptions_idx MATCH "sample" ORDER BY rank;
+-- SELECT rank, t.created_on, t.audio_file_name, t.transcription FROM transcriptions t
+-- JOIN transcriptions_idx i ON t.ROWID = i.ROWID
+-- WHERE transcriptions_idx MATCH "sample" ORDER BY rank;
